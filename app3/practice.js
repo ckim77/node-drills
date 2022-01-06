dd394493a41b05201078aa7efb42737c15432ab6
Chris Kim <ckjr92@gmail.com>
2022-01-06 16:07:53 -0700

    finished app3

--- app3/practice.js
+++ app3/practice.js
@@ -1,3 +1,18 @@
 const addTwoFunction = require('./addTwoFunction.js');
 
-console.log(addTwoFunction(7));
+// console.log(addTwoFunction(7));
+
+
+// destructuring several functions
+const { minusOne, timesThree, ageText } = require('./functions.js');
+
+// console.log(minusOne(3));
+// console.log(timesThree(2));
+// console.log(ageText(29));
+
+
+// method way to call functions
+
+// const func = require('./functions.js');
+// console.log(func.ageText(234));
+
--- app3/practice.mjs
+++ app3/practice.mjs
@@ -1,2 +1,12 @@
 import addTwoFunction from './addTwoFunction.js';
-console.log(addTwoFunction(6))
+// console.log(addTwoFunction(6))
+
+import functions from './functions.js';
+// const { ageText } = functions;
+
+// console.log(ageText(3))
+
+// console.log(functions.minusOne(4));
+
+
+
